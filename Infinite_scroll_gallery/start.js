45fa2851dfc20371e0ccceeeb09b8157d1460075
Zeekerz <pramote.p.on@gmail.com>
2021-03-04 21:15:42 +0700

    21 days challennge End

---
+++ Infinite_scroll_gallery/start.js
@@ -0,0 +1,41 @@
+(() => {
+    const KEY = 'ddG4MZXG_tUP0PgkPVazgysvFNfla6rFZPzDDHc55O4';
+    const loaderElem = document.querySelector('.loader');
+    let page = 1;
+
+    function showLoader(){
+        loaderElem.classList.add('visible');
+    }
+    function hideLoader(){
+        loaderElem.classList.remove('visible');
+    }
+    async function displayImages(){
+
+        showLoader();
+        const result = await fetch(`https://api.unsplash.com/photos/?client_id=${KEY}&page=${page}`);
+        const images = await result.json();
+
+        const galleryElem = document.querySelector('.gallery');
+
+        images.forEach(image => {
+           const imgElem = document.createElement('img');
+           imgElem.src =  image.urls.small;
+           galleryElem.appendChild(imgElem);
+        });
+
+        hideLoader();
+
+        page += 1;
+    }
+    function onScroll(){
+        const {scrollTop, clientHeight, scrollHeight} = document.documentElement;
+        if(scrollTop + clientHeight >= scrollHeight -10){
+            displayImages();
+        }
+    }
+    function run(){
+        document.addEventListener('scroll', onScroll);
+        displayImages();
+    }
+    run();
+})();
